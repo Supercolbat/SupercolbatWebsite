e18523a5d05ae2e848b8dd10a6580eb5c9f79f7e
Supercolbat <supercolbat@gmail.com>
2021-01-22 16:55:43 +0000

    Fixed improper indents

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
 function animateScale(element, scale, duration, elasticity) {
     anime.remove(element);
     anime({
-      targets: element,
-      scale: scale,
-      duration: duration,
-      elasticity: elasticity
+        targets: element,
+        scale: scale,
+        duration: duration,
+        elasticity: elasticity
     });
 }
 
@@ -24,15 +24,15 @@ $(function() {
         for (var i = 0; i < data.length; i+=3) {
             chunk = data.slice(i, i+3);
 
-             html = `<div class="row">`;
-             for (var k = 0; k < chunk.length; k++) {
-                 html = html + `<div class="col"><h3>${data[i+k].url ? `<a href="${data[i+k].url} target="_blank">${data[i+k].name}</a>` : data[i+k].name} ${data[i+k].tags.includes("wip") ? ` <span class="badge badge-warning">WIP</span>` : ""}</h3><p>${data[i+k].description}</p></div>`;
-                 
-                 if (data[i+k].tags.includes("wip")) {
-                     unfinished++;
-                 }
-             }
-             $("#programs").append(html + "</div>");
+            html = `<div class="row">`;
+            for (var k = 0; k < chunk.length; k++) {
+                html = html + `<div class="col"><h3>${data[i+k].url ? `<a href="${data[i+k].url} target="_blank">${data[i+k].name}</a>` : data[i+k].name} ${data[i+k].tags.includes("wip") ? ` <span class="badge badge-warning">WIP</span>` : ""}</h3><p>${data[i+k].description}</p></div>`;
+                
+                if (data[i+k].tags.includes("wip")) {
+                    unfinished++;
+                }
+            }
+            $("#programs").append(html + "</div>");
         }
 
         $("#counter").text(unfinished);
