ce601b229e4164475670d0ab97f19c93b36f57a2
Supercolbat <supercolbat@gmail.com>
2021-01-22 16:59:17 +0000

    Missing quote broke links

--- scripts/index.js
+++ scripts/index.js
@@ -26,7 +26,7 @@ $(function() {
 
             html = `<div class="row">`;
             for (var k = 0; k < chunk.length; k++) {
-                html = html + `<div class="col"><h3>${data[i+k].url ? `<a href="${data[i+k].url} target="_blank">${data[i+k].name}</a>` : data[i+k].name} ${data[i+k].tags.includes("wip") ? ` <span class="badge badge-warning">WIP</span>` : ""}</h3><p>${data[i+k].description}</p></div>`;
+                html = html + `<div class="col"><h3>${data[i+k].url ? `<a href="${data[i+k].url}" target="_blank">${data[i+k].name}</a>` : data[i+k].name} ${data[i+k].tags.includes("wip") ? ` <span class="badge badge-warning">WIP</span>` : ""}</h3><p>${data[i+k].description}</p></div>`;
                 
                 if (data[i+k].tags.includes("wip")) {
                     unfinished++;
